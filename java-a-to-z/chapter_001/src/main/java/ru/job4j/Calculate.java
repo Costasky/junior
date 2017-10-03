64629ade1c0b13ca32d4265beb0723056726d33f
Costasky <tsk6613881952@gmail.com>
2017-10-03 11:43:08 -0700

    package-info.java

--- java-a-to-z/chapter_001/src/main/java/ru/job4j/Calculate.java
+++ java-a-to-z/chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,5 +1,17 @@
 package ru.job4j;
+
+/**
+*Class Calculate решение задачи части 001 урок1.
+*author thay
+*@since 9.30.2017
+*/
 public class Calculate {
+
+/**
+*This is a method for testing
+*@param value line for output console 
+*@return String value.
+*/
   public String echo(String value) {
    return String.format ("%s %s %s" , value, value, value);
 }
